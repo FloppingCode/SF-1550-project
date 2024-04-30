b4d46473c52698bf2b071b61de7e36e5be777905
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-04-30 16:09:54 +0200

    Update gaussnewton.m

--- gaussnewton.m
+++ gaussnewton.m
@@ -1,27 +1,35 @@
-function [x, y, a] = gaussnewton(omega, eta_values, Ic_list, alpha_list, x_tilde, y_tilde, a_tilde)
-    max_iter = 100;
-    tol = 1e-10;
-    J = zeros(length(alpha_list), 3);
-    new = [x_tilde; y_tilde; a_tilde];
+function [x_tilde, y_tilde, a_tilde, it] = gaussnewton(eta, ic_vals, aa_vals, omega, x0, y0, a0)
 
-    for iter = 1:max_iter
-        old = new;
-        
-        F = (new(1) .* cos(new(2) .* cos(alpha_list) + new(3) .* sin(alpha_list))) - Ic_list;
-        
-        J(:, 1) = cos(new(2) .* cos(alpha_list) + new(3) .* sin(alpha_list));
-        J(:, 2) = -new(1) .* cos(alpha_list) .* sin(new(2) .* cos(alpha_list) + new(3) .* sin(alpha_list));
-        J(:, 3) = -new(1) .* sin(alpha_list) .* sin(new(2) .* cos(alpha_list) + new(3) .* sin(alpha_list));
+maxiter = 1000;
+tol = 1e-4;
+it = 0;
 
-        d = J \ (-F);
-        new = old + d;
+for i = 1:maxiter
+    old = [omega*x0 ; omega*y0 ; eta*a0];
+    cos_vals = cos(old(1) .* cos(aa_vals) + old(2) .* sin(aa_vals));
+    I_c = old(3) .* cos_vals - ic_vals;
+    J = jac(old(1), old(2), old(3), aa_vals);
+    d = J \ (-I_c);
 
-        if norm(old - new) < tol
-            break;
-        end
+    new = old + d;
+    it = it + 1;
+    if norm(old - new) < tol
+        break
     end
-
-    a = new(1) / eta_values;
-    x = new(2) / omega;
-    y = new(3) / omega;
 end
+
+x_tilde = new(1, 1) / omega;
+y_tilde = new(2, 1) / omega;
+a_tilde = new(3, 1) / eta;
+
+end
+
+function J = jac(x0, y0, a0, aa_vals)
+
+dx = (-sin(x0 .* cos(aa_vals) + y0 .* sin(aa_vals)) .* a0 .* cos(aa_vals))';
+dy = (-sin(x0 .* cos(aa_vals) + y0 .* sin(aa_vals)) .* a0 .* sin(aa_vals))';
+da = (cos(x0 .* cos(aa_vals) + y0 .* sin(aa_vals)))';
+
+J = [dx', dy', da'];
+end
+
