2d0a789fb0ddb6ccc201f2cabbfe89632f62f14b
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-04-30 15:48:54 +0200

    added alpha as input arg

--- trapets2d.m
+++ trapets2d.m
@@ -1,4 +1,4 @@
-function integral = trapets2d(func, x_min, x_max, y_min, y_max, num_points)
+function [integral] = trapets2d(func, x_min, x_max, y_min, y_max, num_points, a)
     dx = (x_max - x_min) / num_points;
     dy = (y_max - y_min) / num_points;
     integral = 0;  
@@ -6,7 +6,7 @@ function integral = trapets2d(func, x_min, x_max, y_min, y_max, num_points)
         for j = 1:num_points
             x = x_min + (i - 1) * dx;
             y = y_min + (j - 1) * dy;
-            integral = integral + func(x, y);
+            integral = integral + func(x, y, a); 
         end
     end
     integral = integral * dx * dy;
