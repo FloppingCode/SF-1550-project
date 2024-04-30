e0d6ac90f9cd0b11e071a223959c680b549dc45b
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-04-30 16:10:11 +0200

    Update gaussnewton.m

--- gaussnewton.m
+++ gaussnewton.m
@@ -5,7 +5,7 @@ tol = 1e-4;
 it = 0;
 
 for i = 1:maxiter
-    old = [omega*x0 ; omega*y0 ; eta*a0];
+    old = [omega*x0; omega*y0; eta*a0];
     cos_vals = cos(old(1) .* cos(aa_vals) + old(2) .* sin(aa_vals));
     I_c = old(3) .* cos_vals - ic_vals;
     J = jac(old(1), old(2), old(3), aa_vals);
