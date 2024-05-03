5f8e94cd36551c595617dde41c963a458db48985
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-05-03 16:40:02 +0200

    Update simpson.m

--- simpson.m
+++ simpson.m
@@ -1,17 +1,23 @@
-function V = simpson(x, y, g, a, omega, bound)
+function integral_value = simpson(x, y, g, a, omega, bound)
+% Funktion för att beräkna integralen av en funktion över ett givet intervall
+% med Simpsons regel.
+
     n = length(x);
     vc = @(x, y, a, omega) cos(omega * (x * cos(a) + y * sin(a)));
     f = @(x, y, a, omega, g) vc(x, y, a, omega) * g;
     h = bound(2) - bound(1);
+    
+    % Beräkning av de första och sista termerna i Simpsons regel
     s = f(x(1), y(1), a, omega, g(1)) + f(x(end), y(end), a, omega, g(end)); 
     
+    % Loopa över de ojämna termera 
     for i = 3:2:(n-1)
         s = s + 4 * f(x(i), y(i), a, omega, g(i)); 
     end
+    
+    % Loopa över de jämna termerna
     for i = 2:2:(n-1)
         s = s + 2 * f(x(i), y(i), a, omega, g(i));
     end
-    
-    V = h / 3 * s;
+        integral_value = h / 3 * s;
 end
-
