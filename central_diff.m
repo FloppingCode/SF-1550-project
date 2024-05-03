c6a5c54290edafc22480d6007e12640e70ab31a8
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-05-03 16:38:24 +0200

    Update central_diff.m

--- central_diff.m
+++ central_diff.m
@@ -1,4 +1,6 @@
 function ic_diff = central_diff(x, y, g, al, omega, bound, h)
+% Funktiom som uppskattar derivatan med hjälp av I_c med hjälp av
+% centraldifferens
     ic_plus = simpson(x, y, g, al + h, omega, bound);
     ic_minus = simpson(x, y, g, al - h, omega, bound);
     ic_diff = (ic_plus - ic_minus) / (2*h);
