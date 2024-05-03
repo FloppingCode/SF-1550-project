4af5766b0f8a8d871981ba90b1cb501091882280
Alissa Pandikow <146581176+alissapandikow@users.noreply.github.com>
2024-05-03 16:39:48 +0200

    Update noise_error.m

--- noise_error.m
+++ noise_error.m
@@ -1,17 +1,24 @@
 function [noiselevel, err_total, err_x, err_y, err_a] = noise_error(Bound, g, omega, eta, xs, ys, a, x_tilde, y_tilde, a_tilde)
-    M = 15;
+% Funktion som beräknar felen, både totalt och för varje parameter, när
+% brus läggs till 
+
+    % Intervall för brusnivån
     noiselevel = linspace(0.01, 1);
+    
     err_total = zeros(length(noiselevel), 1);
     err_x = zeros(length(noiselevel), 1);
     err_y = zeros(length(noiselevel), 1);
     err_a = zeros(length(noiselevel), 1);
 
+    M = 15;
     aa_vals = linspace(0, 2*pi, M)';
     ic_vals = zeros(M, 1);
 
     for j = 1:length(noiselevel)
+        % Lägg till brus till ljudkällan
         gnoise = g + max(abs(g)) * randn(size(g)) * noiselevel(j);
 
+        % Beräkna Ic för brusig ljudkälla
         for k = 1:M
             ic_vals(k) = simpson(Bound.x, Bound.y, gnoise, aa_vals(k), omega, Bound.s);
         end
@@ -21,7 +28,6 @@ function [noiselevel, err_total, err_x, err_y, err_a] = noise_error(Bound, g, om
         err_x(j) = abs(x0 - xs);
         err_y(j) = abs(y0 - ys);
         err_a(j) = abs(a0 - a);
-
         err_total(j) = sqrt(err_x(j)^2 + err_y(j)^2 + err_a(j)^2);
     end
 end
